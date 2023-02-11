c82d88f0842954a48af37120e9451dc971fd2976
kim <gksqlsk122@naver.com>
2023-02-11 13:55:03 +0900

    임플

--- src/team3/groupware5/service/EmployeeService.java
+++ src/team3/groupware5/service/EmployeeService.java
@@ -13,8 +13,34 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeDAO employeedao;
 
+<<<<<<< Updated upstream
 	public boolean getLogin(String email, String password) throws Exception {
 		return employeedao.FindLogin(email, password);
 	}
+=======
+	
+	public boolean getLogin(String email,String password) throws Exception {
+		return employeedao.FindLogin(email, password);
+	}
+
+	
+	
+	
+	
+	
+//	public boolean getEmployee(String email, String password) throws Exception {
+//		try {
+//			employeedao.findEmployee(email, password);
+//			System.out.println("080808");
+//			
+//		}catch(Exception e) {
+//			System.out.println("++++++++++++++++++++++++++++++++");
+//			e.printStackTrace();
+//			throw new Exception("id pw invalidate입니다");
+//		}
+//		System.out.println("000000----------------------------");
+//		return true;
+//	}
+>>>>>>> Stashed changes
 
 }
